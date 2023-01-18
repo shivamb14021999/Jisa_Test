98eee103ad1a3d81f136f4efb6507df608c8c90b
shivamb14021999 <shivamborasi1402@gmail.com>
2023-01-18 15:14:39 +0530

    For test

---
+++ src/main/java/com/example/demo/DemoSpring1Application.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpring1Application.class, args);
+		System.out.print(false);
+		///aaaaaaaa
+	}
+
+}
